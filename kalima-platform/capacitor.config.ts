e35168e575a74c969bb93efa56d45e75d8533041
ahmedelmahd <ahmedsamir120495@gmail.com>
2025-02-20 10:38:08 +0200

    CHANGE EVERYTHING

--- kalima-platform/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'kalima-platform',
-  webDir: 'build'
-};
-
-export default config;
