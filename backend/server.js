9e9dde94e82cb872256bca92cc4df16f780e9c2b
Youssef25331 <yousseft25331@gmail.com>
2025-03-04 17:48:50 +0200

    Added authentication routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3200;
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cors(corsOptions));
+
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/register", require("./routes/registerRoutes.js"));
+app.use("/auth", require("./routes/authRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`Server active and listening on port ${PORT}`);
