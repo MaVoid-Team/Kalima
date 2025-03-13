7f523caaf9c1bde9446968df9d3b20d5286cf278
Youssef25331 <yousseft25331@gmail.com>
2025-03-13 21:40:56 +0200

    Updated joi validation to be usable when updating a user

--- kalima-platform/backend/controllers/userController.js
+++ kalima-platform/backend/controllers/userController.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose')
 const registerController = require('../controllers/registerController')
 const User = require("../models/userModel.js");
+const Parent = require("../models/parentModel.js");
+const Lecturer = require("../models/lecturerModel.js");
+const Student = require("../models/studentModel.js");
+const Teacher = require("../models/teacherModel.js");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const bcrypt = require("bcrypt");
@@ -17,24 +20,45 @@ const getUser = catchAsync(async (req, res, next) => {
   if (!user) return next(new AppError("Couldn't find user.", 404));
   res.json(user);
 })
+
 const createUser = registerController.registerNewUser;
 
 const updateUser = catchAsync(async (req, res, next) => {
-  const { name, email, phoneNumber, password } = req.body
+  const { name, email, address, password } = req.body
+  const userId = req.params.userId
+
   let hashedPwd
+
   if (password) {
     hashedPwd = await bcrypt.hash(password, 12);
   }
-  const foundUser = await User.findByIdAndUpdate(
-    req.params.userId,
-    { name, email, phoneNumber, password: hashedPwd }, {
-    new: true,
-    runValidators: true,
-  }).select("-password")
+  const foundUser = await User.findById(userId).select("-password")
+
   if (!foundUser) return next(new AppError("User not found", 404));
 
-  res.json(foundUser);
+  const updatedUser = { name, email, address, password, ...req.body }
+  let user
+
+  switch (foundUser.role.toLowerCase()) {
+    case "teacher":
+      user = await Teacher.findByIdAndUpdate(userId, updatedUser, { new: true, runvalidators: true }).select("-password").lean()
+      break;
+    case "student":
+      user = await Student.findByIdAndUpdate(userId, updatedUser, { new: true, runValidators: true }).select("-password").lean()
+      break;
+    case "parent":
+      user = await Parent.findByIdAndUpdate(userId, updatedUser, { new: true, runValidators: true }).select("-password").lean()
+      break;
+    case "lecturer":
+      user = await Lecturer.findByIdAndUpdate(userId, updatedUser, { new: true, runValidators: true }).select("-password").lean()
+      break;
+    default:
+      return next(new AppError("Invalid role", 400));
+  }
+
+  res.json(user)
 })
+
 const deleteUser = catchAsync(async (req, res, next) => {
   const foundUser = await User.findByIdAndDelete(req.params.userId).select("-password").lean()
   if (!foundUser) return next(new AppError("User not found", 404));
--- kalima-platform/backend/routes/userRoutes.js
+++ kalima-platform/backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const validateUser = require("../middleware/validateUser")
 
+router.route("/").get(userController.getAllUsers).post(validateUser, userController.createUser)
 
-router.route("/").get(userController.getAllUsers).post(userController.createUser)
-
-router.route("/:userId").get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
+router.route("/:userId").get(userController.getUser).patch(validateUser, userController.updateUser).delete(userController.deleteUser)
 
 module.exports = router
