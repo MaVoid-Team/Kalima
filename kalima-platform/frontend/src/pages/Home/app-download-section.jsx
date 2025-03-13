30faa85403269546493543caac6ed4bc624cafd0
ahmedelmahd <ahmedsamir120495@gmail.com>
2025-03-13 07:24:52 +0200

    adjusted responsiveness in landing page

--- kalima-platform/frontend/src/pages/Home/app-download-section.jsx
+++ kalima-platform/frontend/src/pages/Home/app-download-section.jsx
@@ -1,10 +1,10 @@
 export function AppDownloadSection() {
   return (
-    <section className="border-primary border-2 rounded-full w-3/4 mx-auto p-8">
+    <section className="border-primary border-2 rounded-full md:w-3/4 w-full mx-auto md:p-8">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
           {/* Image on the left */}
-          <div className="flex justify-center order-2 md:order-1">
+          <div className="flex justify-center md:order-1">
             <div className="relative">
               <img src="/qr-code.png" alt="Mobile App" className="h-80 object-contain hover:scale-125 transition-all duration-500" />
               <div className="absolute -top-4 -right-4 w-12 h-12 border-2 border-dashed border-primary rounded-full"></div>
