1f67e96bd0bc5c0a368626720949310eed5bfdd4
Youssef25331 <yousseft25331@gmail.com>
2025-03-13 16:14:27 +0200

    Added Controllers and Routes for users.

---
+++ kalima-platform/backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+
+router.route("/").get(userController.getAllUsers).post(userController.createUser)
+
+router.route("/:userId").get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
+
+module.exports = router
