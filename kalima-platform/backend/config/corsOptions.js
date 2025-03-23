da085ccbb715c5fec84e3953fa203d0380969195
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2025-03-23 12:55:22 +0200

    Update allowedOrigins to use HTTPS and improve CORS error handling

--- kalima-platform/backend/config/corsOptions.js
+++ kalima-platform/backend/config/corsOptions.js
@@ -1,23 +1,24 @@
 const allowedOrigins = require("./allowedOrigins");
 
+function checkOrigin(origin) {
+  // If origin is provided and it's not in allowedOrigins,
+  // the expression on the left of the OR operator is false,
+  // causing the immediately invoked function expression (IIFE)
+  // to execute and throw an error.
+  (allowedOrigins.indexOf(origin) !== -1 || !origin) ||
+    (() => { throw new Error("Not allowed by CORS"); })();
+}
+
 const corsOptions = {
   origin: (origin, callback) => {
     try {
-      if (
-        allowedOrigins.indexOf(origin) !== -1 ||
-        !origin // No origin accepted in case of using local testing software
-      ) {
-        callback(null, true);
-      } else {
-        // return the error message
-        callback(new Error("Not allowed by CORS"));
-      }
+      checkOrigin(origin);
+      callback(null, true);
     } catch (err) {
       console.error("CORS validation error:", err);
-      callback(new Error("CORS error occurred"));
+      callback(err);
     }
   },
-
   credentials: true,
   optionsSuccessStatus: 200,
 };
