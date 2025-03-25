e06931461644422b9d0956ca177e281170d96e72
Youssef25331 <yousseft25331@gmail.com>
2025-03-25 16:52:08 +0200

    Lecture attachment routes and controllers

--- kalima-platform/backend/routes/lectureRoutes.js
+++ kalima-platform/backend/routes/lectureRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const lectureController = require("../controllers/lectureController");
+const attachmentController = require('../controllers/attachmentController')
 
 const router = express.Router();
 
@@ -13,6 +14,16 @@ router
   .get(lectureController.getLectureById)
   .patch(lectureController.updatelectures)
   .delete(lectureController.deletelecture);
+
+router
+  .route('/attachments/:lectureId')
+  .get(attachmentController.getLectureAttachment)
+  .post(attachmentController.upload.single('attachment'),
+    attachmentController.createAttachment)
+
+router
+  .route('/attachment/:attachmentId')
+  .get(attachmentController.getAttachmentFile)
 
 router
   .route("/lecturer/:lecturerId")
