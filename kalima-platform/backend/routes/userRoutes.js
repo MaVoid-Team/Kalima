08b2584455154c987c99e722af1adc4c6f9c2522
MahmoudMuhammad <mahmoudmuhammadd0@gmail.com>
2025-04-01 17:20:50 +0200

    feat: :white_check_mark: Implemented function that responsible for bulk creation

--- kalima-platform/backend/routes/userRoutes.js
+++ kalima-platform/backend/routes/userRoutes.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const validateUser = require("../middleware/validateUser")
-const verifyJWT = require("../middleware/verifyJWT")
+const validateUser = require("../middleware/validateUser");
+const verifyJWT = require("../middleware/verifyJWT");
+const uploadFileMiddleware =
+  require("../utils/upload files/uploadFiles").uploadFileMiddleware;
 
-router.route("/").get(userController.getAllUsers).post(validateUser, userController.createUser)
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(validateUser, userController.createUser);
 
-router.route("/:userId").get(userController.getUser).patch(validateUser, userController.updateUser).delete(userController.deleteUser)
+router
+  .route("/:userId")
+  .get(userController.getUser)
+  .patch(validateUser, userController.updateUser)
+  .delete(userController.deleteUser);
 
-router.route("/role/:role").get(userController.getAllUsersByRole)
+router.route("/role/:role").get(userController.getAllUsersByRole);
 
-router.route("/update/password").patch(verifyJWT,userController.changePassword)
+router
+  .route("/update/password")
+  .patch(verifyJWT, userController.changePassword);
+router
+  .route("/accounts/bulk-create")
+  .post(uploadFileMiddleware, userController.uploadFileForBulkCreation);
 
-module.exports = router
+module.exports = router;
