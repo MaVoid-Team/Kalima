7b1720f6f6c7f5fb1e97cf0236e65ed1a8c7dfb3
Karim Gad <karimmohamedgad21@gmail.com>
2025-04-20 21:11:41 +0200

    feat: update role verification middleware to include lecturer role

--- kalima-platform/backend/routes/levelRoutes.js
+++ kalima-platform/backend/routes/levelRoutes.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.use(verifyJWT, authController.verifyRoles("admin", "subadmin", "moderator"));
+router.use(verifyJWT, authController.verifyRoles("admin", "subadmin", "moderator", "lecturer"));
 
 router
   .route("/")
