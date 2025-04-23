05b28989f24cee44ceac2de093e6961d27cd3b95
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2025-04-23 14:48:42 +0200

    refactor: simplify CORS configuration by removing origin check function

--- kalima-platform/backend/config/corsOptions.js
+++ kalima-platform/backend/config/corsOptions.js
@@ -1,25 +1,8 @@
 const allowedOrigins = require("./allowedOrigins");
 
-function checkOrigin(origin) {
-  // If origin is provided and it's not in allowedOrigins,
-  // the expression on the left of the OR operator is false,
-  // causing the immediately invoked function expression (IIFE)
-  // to execute and throw an error.
-  (allowedOrigins.indexOf(origin) !== -1 || !origin) ||
-    (() => { throw new Error("Not allowed by CORS"); })();
-}
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    try {
-      checkOrigin(origin);
-      callback(null, true);
-    } catch (err) {
-      console.error("CORS validation error:", err);
-      callback(err);
-    }
-  },
-
+  origin: allowedOrigins,
   credentials: true,
   optionSuccessStatus: 200,
 };
