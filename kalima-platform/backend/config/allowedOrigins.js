f0ddb0d21499833d36f8dce5e3439504af5a4da1
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2025-04-24 16:36:16 +0200

    fix: uncomment localhost:3000 in allowedOrigins for clarity

--- kalima-platform/backend/config/allowedOrigins.js
+++ kalima-platform/backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost',         // Android HTTP
     'https://localhost',        // Android HTTPS 
     'https://kalima-edu.com',
-    // 'http://localhost:3000',
+    'http://localhost:3000',
 ];
 
 module.exports = allowedOrigins;
