996008e021868c77fa91c43fb4d29f44401420c5
Abdelrhman Ahmed <abdelrhmanahmed0024@gmail.com>
2025-11-10 19:47:40 +0200

    access modifycation

--- kalima-platform/backend/routes/subjectRoutes.js
+++ kalima-platform/backend/routes/subjectRoutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const subjectController = require("../controllers/subjectController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router
   .route("/")
-  .post(subjectController.createSubject)
+  .post(authController.verifyRoles("Admin", "SubAdmin", "Moderator"), subjectController.createSubject)
   .get(subjectController.getAllSubjects);
 
-router.route("/:id/level").patch(subjectController.updateLevelOfSubject);
+router.route("/:id/level").patch(authController.verifyRoles("Admin", "SubAdmin", "Moderator"), subjectController.updateLevelOfSubject);
 router
   .route("/:id")
   .get(subjectController.getSubjectById)
-  .patch(subjectController.updateSubjectById)
-  .delete(subjectController.deleteSubjectById);
+  .patch(authController.verifyRoles("Admin", "SubAdmin", "Moderator"), subjectController.updateSubjectById)
+  .delete(authController.verifyRoles("Admin", "SubAdmin"), subjectController.deleteSubjectById);
 
 
 
