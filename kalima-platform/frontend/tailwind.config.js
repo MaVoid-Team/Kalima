c930dd94c464072b307fd48fb914919531543c81
Ziad Ahmed <Ziad.Ahmed.25.25.25@gmail.com>
2026-01-20 14:40:25 +0200

    maybe

--- kalima-platform/frontend/tailwind.config.js
+++ kalima-platform/frontend/tailwind.config.js
@@ -6,31 +6,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
       animation: {
-      'bounce-slow': 'bounce-slow 3s ease-in-out infinite',
-    },
-    keyframes: {
-      'bounce-slow': {
-        '0%, 100%': { 
-          transform: 'translateY(-25%)',
-          animationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
-        },
-        '50%': { 
-          transform: 'translateY(0)',
-          animationTimingFunction: 'cubic-bezier(0,0,0.2,1)'
-        },
-      }
+        'bounce-slow': 'bounce-slow 3s ease-in-out infinite',
+      },
+      keyframes: {
+        'bounce-slow': {
+          '0%, 100%': { 
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
+          },
+          '50%': { 
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)'
+          },
+        }
+      },
     },
   },
-  },
-  plugins: [require("@tailwindcss/postcss"),
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: [],
-  
-    base: true,         
-    styled: true,       
-    utils: true,      
+    themes: ["light", "dark"],
+    base: true,
+    styled: true,
+    utils: true,
   },
 }
