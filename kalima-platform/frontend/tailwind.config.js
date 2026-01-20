68a54de5c32e3f28d567255c87df6720f10ff847
Ziad Ahmed <Ziad.Ahmed.25.25.25@gmail.com>
2026-01-20 14:41:46 +0200

    Revert "maybe"
    This reverts commit 231ec75d6785eec31af502e5b5f6c6dfb3f46f4f.

--- kalima-platform/frontend/tailwind.config.js
+++ kalima-platform/frontend/tailwind.config.js
@@ -6,28 +6,31 @@ module.exports = {
   ],
   theme: {
     extend: {
+      
       animation: {
-        'bounce-slow': 'bounce-slow 3s ease-in-out infinite',
-      },
-      keyframes: {
-        'bounce-slow': {
-          '0%, 100%': { 
-            transform: 'translateY(-25%)',
-            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
-          },
-          '50%': { 
-            transform: 'translateY(0)',
-            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)'
-          },
-        }
-      },
+      'bounce-slow': 'bounce-slow 3s ease-in-out infinite',
+    },
+    keyframes: {
+      'bounce-slow': {
+        '0%, 100%': { 
+          transform: 'translateY(-25%)',
+          animationTimingFunction: 'cubic-bezier(0.8,0,1,1)'
+        },
+        '50%': { 
+          transform: 'translateY(0)',
+          animationTimingFunction: 'cubic-bezier(0,0,0.2,1)'
+        },
+      }
     },
   },
-  plugins: [require("daisyui")],
+  },
+  plugins: [require("@tailwindcss/postcss"),
+  ],
   daisyui: {
-    themes: ["light", "dark"],
-    base: true,
-    styled: true,
-    utils: true,
+    themes: [],
+  
+    base: true,         
+    styled: true,       
+    utils: true,      
   },
 }
