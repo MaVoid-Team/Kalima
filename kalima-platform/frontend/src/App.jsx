9755ac661e91a45536badb96e222b89449a74a54
AbdelrahmanHabib24 <abdelrahmanhabib502@gmail.com>
2026-02-04 23:42:21 +0200

    Implement initial frontend application structure including a landing page, navigation, and footer.

--- kalima-platform/frontend/src/App.jsx
+++ kalima-platform/frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import LandingPage from './pages/landing/LandingPage';
 import { Toaster } from 'sonner';
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<LandingPage />} />
+        </Route>
       </Routes>
       <Toaster position="bottom-right" richColors />
     </Router>
---
+++ kalima-platform/frontend/src/pages/landing/LandingPage.jsx
@@ -0,0 +1,13 @@
+import WelcomeSection from "../../components/landing/WelcomeSection";
+import SubjectsSection from "../../components/landing/SubjectsSection";
+import FeaturedBooklets from "../../components/landing/FeaturedBooklets";
+
+export default function LandingPage() {
+  return (
+    <>
+      <WelcomeSection />
+      <SubjectsSection />
+      <FeaturedBooklets />
+    </>
+  );
+}
