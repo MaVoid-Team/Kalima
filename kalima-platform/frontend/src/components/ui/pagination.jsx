8548692146d05451656d4ceec68fb005c7d25207
ahmedelmahd <ahmedsamir120495@gmail.com>
2026-02-24 05:47:36 +0200

    smol adjustment to pagination component for rtl support

--- kalima-platform/frontend/src/components/ui/pagination.jsx
+++ kalima-platform/frontend/src/components/ui/pagination.jsx
@@ -61,7 +61,7 @@ function PaginationLink({
 
 function PaginationPrevious({
   className,
-  text = "Previous",
+  text,
   ...props
 }) {
   return (
@@ -78,7 +78,7 @@ function PaginationPrevious({
 
 function PaginationNext({
   className,
-  text = "Next",
+  text,
   ...props
 }) {
   return (
