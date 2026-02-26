b16af9141b9f82d559e91913bed9b7b8a4ed01e2
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2026-02-26 03:16:27 +0200

    coupons

--- kalima-platform/backend/src/apps/store-api/controllers/coupon.controller.ts
+++ kalima-platform/backend/src/apps/store-api/controllers/coupon.controller.ts
@@ -105,8 +105,41 @@ export const couponController = {
     _next: NextFunction,
   ): Promise<void> {
     try {
-      const dto = await validateDto(getAllCouponsDto, req.query);
-      const result = await couponService.getAllCoupons(dto);
+      const active =
+        req.query.active !== undefined ? req.query.active === "1" : undefined;
+
+      const isAmount =
+        req.query.isAmount !== undefined
+          ? req.query.isAmount === "1"
+          : undefined;
+
+      const startDate = req.query.startDate
+        ? new Date(req.query.startDate as string)
+        : undefined;
+
+      const endDate = req.query.endDate
+        ? new Date(req.query.endDate as string)
+        : undefined;
+
+      const page = req.query.page ? parseInt(req.query.page as string, 10) : 1;
+
+      const limit = req.query.limit
+        ? parseInt(req.query.limit as string, 10)
+        : 50;
+
+      const product_id = req.query.product_id
+        ? parseInt(req.query.product_id as string, 10)
+        : undefined;
+
+      const result = await couponService.getAllCoupons({
+        page,
+        limit,
+        product_id,
+        active,
+        isAmount,
+        startDate,
+        endDate,
+      });
 
       res.status(200).json({
         success: true,
