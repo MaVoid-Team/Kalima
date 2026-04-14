c03e613d03a06b72749ccd9a80e77a3c1c0870b7
zoaa3054 <zoaa3054@gmail.com>
2026-04-14 21:47:12 +0200

    feat: trying v2.0 to enhance the mobile performance to more than 80%. This fixes #354

--- kalima-platform/frontend/vite.config.js
+++ kalima-platform/frontend/vite.config.js
@@ -2,11 +2,12 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,5 +17,20 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
     cssMinify: true,
     cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('firebase')) {
+            return 'firebase-bundle';
+          }
+          if (id.includes('framer-motion') || id.includes('lucide-react') || id.includes('lenis') || id.includes('radix-ui')) {
+            return 'ui-frameworks';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
 })
